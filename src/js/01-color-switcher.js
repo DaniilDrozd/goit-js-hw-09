845f082bb734304723217512860b248cfc0947e2
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-30 18:45:07 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+
+let intervalId = null;
+
+
+  btnStart.addEventListener('click',StartSwColor);
+  btnStop.addEventListener('click',StopSwColor);
+
+
+  function StartSwColor(){
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    intervalId = setInterval( getRandomHexColor,1000);
+}
+  
+ function StopSwColor(){
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+
+    clearInterval(intervalId);
+ }
+
+ function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
