732081e086f8907598f6d9237cce12b0e3f94bd7
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-30 15:45:34 +0000

    Deploying to gh-pages from @ DaniilDrozd/goit-js-hw-09@845f082bb734304723217512860b248cfc0947e2 🚀

---
+++ 01-color-switcher.9c3a0527.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=(document.querySelector("body"),null);function a(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.9c3a0527.js.map
