d18f57713f4e5ce2b4ea699afe1878dc778801b6
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-07-03 20:31:59 +0300

    end task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const delay = Number(form.delay.value);
+  for (let i = 1; i <= form.amount.value; i += 1) {
+    const position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position}in${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌  Rejected promise  ${position}in${delay}ms`);
+      });
+  }
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
